6bac046ca0e083add69a194f5cd18c688f929025
Saurav <sauravkumar00108@gmail.com>
2023-10-19 23:17:20 +0530

    changes in socket

--- src/chatDashboard/dashboard.js
+++ src/chatDashboard/dashboard.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../chatDashboard/dashboard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera, faLink, faShare } from "@fortawesome/free-solid-svg-icons"; // Import the solid camera, link, and share icons
+import { faCamera, faLink, faShare } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { messageRoute } from "../utils/api";
+import io from "socket.io-client";
+const socket = io("http://localhost:3000");
 
 function Dashboard() {
   const [message, setMessage] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+  const [sendMessage, setSendMessage] = useState([]);
 
-  const handleMessage = () => {
-    alert(message);
+  useEffect(() => {
+    socket.on("chat message", (message) => {
+      setAllMessages((prevMessages) => [...prevMessages, message]);
+    });
+  }, []);
+
+  const handleMessage = async () => {
+    try {
+      const response = await axios.post(messageRoute, { message });
+      if (response.status === 200) {
+        setSendMessage((prevSendMessages) => [...prevSendMessages, message]);
+        setMessage("");
+      } else {
+        alert("Error sending the message.");
+      }
+    } catch (error) {
+      alert("Message is required");
+    }
   };
 
   const handleInputChange = (event) => {
@@ -18,18 +40,28 @@ function Dashboard() {
     <div className="container">
       <div className="left">
         <div className="header">
-          <h2>Chat Room</h2>
+          <h2 className="headername">Chat Room</h2>
           <FontAwesomeIcon icon={faCamera} className="camicon" />
         </div>
         <hr style={{ backgroundColor: "black", marginTop: "20px" }} />
+        <div className="chatField">
+          {allMessages.map((msg, index) => (
+            <p key={index}>{msg}</p>
+          ))}
+          {sendMessage.map((msg, index) => (
+            <p key={index} className="send-message">
+              {msg}
+            </p>
+          ))}
+        </div>
         <div className="chattype">
           <FontAwesomeIcon icon={faLink} style={{ marginLeft: "5px" }} />
           <input
             type="text"
             placeholder="Write your chat here"
             className="chatinput"
-            value={message} // Set the input value to the 'message' state
-            onChange={handleInputChange} // Handle input changes
+            value={message}
+            onChange={handleInputChange}
           />
           <FontAwesomeIcon
             icon={faShare}
